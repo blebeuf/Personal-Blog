757d1bafc63450cfc377438f2ecf383822465350
blebeuf <bryanlebeuf@gmail.com>
2024-03-05 22:01:52 -0600

    created functions and logged submissions to the console

--- assets/js/blog.js
+++ assets/js/blog.js
@@ -2,4 +2,6 @@
 //     var element = document.body;
 //     element.classList.toggle("dark-mode");
 //   }
-  
+const blogSubmission =  JSON.parse(localStorage.getItem('blogSubmission'));
+console.log(blogSubmission)
+
